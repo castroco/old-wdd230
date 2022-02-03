43cbba7ce8f4436c93176092d51eab8ee58382b3
castroco <carlos.castro.p@gmail.com>
2022-02-03 12:53:38 -0300

    commit

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,3 +1,16 @@
+const menuItems = ["home", "menu1", "menu2"];
+
 function changeClass() {
-    document.querySelector("#menuid").classList.toggle("menuHidden");
+    /*document.querySelector("#menuid").classList.toggle("menuHidden");*/
+    const menu = document.querySelector("#menuid");
+    console.log("menu: ", menu);
+    if (menu.innerHTML == "") {
+        menuItems.forEach(element => {
+            let titleElement = document.createElement("p");
+            titleElement.innerHTML = element;
+            menu.appendChild(titleElement);
+        });
+    } else {
+        menu.innerHTML = "";
+    }
 }
