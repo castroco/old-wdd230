d45d4e32b75ea508670538b0a80a2cfb79e3005a
castroco <carlos.castro.p@gmail.com>
2022-02-04 21:08:16 -0300

    commit

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,4 +1,6 @@
 const menuItems = ["home", "menu1", "menu2", "menu4"];
+const dayOfWeekLong = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const monthLong = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
 
 function changeClass() {
     /*document.querySelector("#menuid").classList.toggle("menuHidden");*/
@@ -14,3 +16,55 @@ function changeClass() {
         menu.innerHTML = "";
     }
 }
+
+function insertDate() {
+    let todayDate = new Date();
+    console.log("todayDate", todayDate);
+    let year = todayDate.getFullYear();
+    let month = todayDate.getMonth();
+    let day = todayDate.getDate();
+    let dayOfWeek = todayDate.getDay();
+    let message = `${dayOfWeekLong[dayOfWeek-1]}, ${day} ${monthLong[month]} ${year}`;
+    const dateContainer = document.querySelector("#timeDiv");
+    dateContainer.innerHTML = message;
+
+    /* Short way- Better way to format the date   */
+    const fulldateUS = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(todayDate);
+    const longdateUS = new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(todayDate);
+    const mediumdateUS = new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(todayDate);
+    const shortdateUS = new Intl.DateTimeFormat("en-US", { dateStyle: "short" }).format(todayDate);
+
+    const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(todayDate);
+    const longdateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "long"}).format(todayDate);
+    const mediumdateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "medium"}).format(todayDate);
+    const shortdateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "short"}).format(todayDate);
+    
+    console.log("fulldate US: ", fulldateUS);
+    console.log("longdate US: ", longdateUS);
+    console.log("mediumdate US: ", mediumdateUS);
+    console.log("shortdate US: ", shortdateUS);
+
+    console.log("fulldate UK: ", fulldateUK);
+    console.log("longdate UK: ", longdateUK);
+    console.log("mediumdate UK: ", mediumdateUK);
+    console.log("shortdate UK: ", shortdateUK);
+}
+
+/*
+function addLine1 () {
+    const yearContainer = document.querySelector("#line1");
+    let message = `&#169; ${getYear()} .:|:. Carlos O. Castro .:|:. Chile`;
+    yearContainer.innerHTML = message;
+}
+function addLine2 () {
+    const updatedContainer = document.querySelector("#line2");
+     document.lastModified;
+    let message = `Last updated: ${document.lastModified}`;
+    updatedContainer.innerHTML = message;
+}
+
+
+addLine1();
+addLine2();
+*/
+insertDate();
