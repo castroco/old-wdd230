4ba49354b8793aa452c53831096f040bfc01dd9f
castroco <carlos.castro.p@gmail.com>
2022-02-22 17:25:52 -0300

    commit

---
+++ lesson8/js/forms.js
@@ -0,0 +1,15 @@
+let form = document.querySelector("form");
+
+let guestName = form.elements.guest;
+let hatSize = form.elements["hat-size"];
+
+/* matches a password input */
+input[type="password"] {}
+
+/* matches a form control whose valid values are limited to a range of values*/
+input[min][max] {}
+
+/* matches a form control with a pattern attribute */
+ input[pattern] {}
+
+ 
